4df2d9a536a31a4e7a157e9b47d61960e2c03549
medoubletea <tuhina_tripathi@hotmail.com>
2024-07-13 09:30:27 +0530

    added products and data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import Footer from "./components/footer/Footer";
+import Slider from "./components/Slider/Slider";
+import Products from "./components/products/Products";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Slider />
+      <Products />
       <Footer />
     </div>
   );
