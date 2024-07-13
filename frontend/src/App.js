b0441a6f6926c7a20ac390bba1b2c67ebf53e821
medoubletea <tuhina_tripathi@hotmail.com>
2024-07-13 09:49:43 +0530

    fashion inspo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
-import Footer from "./components/footer/Footer";
 import Slider from "./components/Slider/Slider";
+import Virtual from "./components/virtual/Virtual"; 
 import Products from "./components/products/Products";
+import Testimonials from "./components/testimonials/Testimonials";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Header />
       <Hero />
       <Slider />
+      <Virtual />
       <Products />
+      <Testimonials />
       <Footer />
     </div>
   );
